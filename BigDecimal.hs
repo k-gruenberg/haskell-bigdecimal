aecef2a73b6e7bcb75cf4407d763075048da3e7f
Kendrick Grünberg <k.gruenberg@tu-braunschweig.de>
2021-04-08 17:14:09 +0200

    still missing are: evalInfiniteSum, 0+0, the mul helper function for (*), the oneOverInt helper function for recip

--- BigDecimal.hs
+++ BigDecimal.hs
@@ -12,16 +12,30 @@
 -- instance RealFrac Double -- Defined in ‘GHC.Float’
 -- instance Show Double -- Defined in ‘GHC.Float’
 -- instance Read Double -- Defined in ‘GHC.Read’
+--
+-- Down below we try to instantiate all of the above for a BigDecimal type with infinitely many digits after the decimal point:
+
+
+-- Please be aware that, due to its infinite decimal expansion, a BigDecimal has the following restrictions compared to the ordinary number types:
+--   * Equality of two BigDecimals can never be proven, only disproven!
+--     This means that (==) will only ever return False but never True and that `compare` will only ever return LT or GT but never EQ.
+--   * One can devide by or calculate the reciprocal of 2.00000... but not 1.99999... (this is due to the infinite series used for reciprocal calculation)
+--   * As of now, one cannot add 0.00000... + 0.00000... for technical reasons (this is something that could possibly be fixed later...)
+
+
 
 module BigDecimal (
   BigDecimal,
   show_N_decimal_places, -- :: Int -> BigDecimal -> String
+  print_BigDecimal,      -- :: BigDecimal -> IO ()
   toRational_precision,  -- :: Int -> BigDecimal -> Rational
   evalInfiniteSum,       -- :: [BigDecimal] -> BigDecimal
   modulo,                -- :: (Num a, Ord a) => a -> a -> a
 ) where
 
 import Data.Ratio ((%),numerator,denominator) -- https://hackage.haskell.org/package/base-4.14.1.0/docs/Data-Ratio.html -- type Rational = Ratio Integer
+import System.IO (hFlush, stdout)             -- used for `print_BigDecimal`
+import Control.Monad (forM_)                  -- used for `print_BigDecimal`
 
 
 
@@ -61,10 +75,10 @@ instance Num Sign where
 
 
 
-
-data BigDecimal = BigDecimal {sign :: Sign, integerPart :: Integer, fractionalPart :: [Int]}
+type DecDigit = Int -- Even though it wastes a lot of space, we're storing decimal digits as Ints.
+data BigDecimal = BigDecimal {sign :: Sign, integerPart :: Integer, fractionalPart :: [DecDigit]}
 -- * Using a scale to specifiy where the decimal point is (like it's done in Java's BigDecimal) wouldn't allow laziness,
---   therefore we're storing the digits after the decimal point as an infinite(always!) lazy list of digits.
+--   therefore we're storing the digits after the decimal point as an infinite(always!) lazy list of (decimal) digits.
 -- * Because 0 == -0 we have to store the sign of a BigDecimal seperately from its integerPart - otherwise we couldn't tell apart +0.1234... and -0.1234... for example!
 --   As a result, we will require that the integerPart shall always be a positive Integer!
 
@@ -76,7 +90,16 @@ show_N_decimal_places n BigDecimal{sign=s, integerPart=i, fractionalPart=f}
 
 
 instance Show BigDecimal where
-  show = show_N_decimal_places 50 
+  show = show_N_decimal_places 50
+
+
+print_BigDecimal :: BigDecimal -> IO ()
+print_BigDecimal BigDecimal{sign=s, integerPart=i, fractionalPart=f} = do
+  putStr $ show s
+  putStr $ show i
+  putStr "."
+  hFlush stdout
+  forM_ f (putStr . show)
 
 
 instance Read BigDecimal where
@@ -107,20 +130,25 @@ instance Ord BigDecimal where
 
 
 instance Enum BigDecimal where
-  toEnum int                                                  = BigDecimal {sign = toEnum $ signum int, integerPart = abs $ toInteger int, fractionalPart = [0,0..]}
-  fromEnum BigDecimal{sign=s, integerPart=i,fractionalPart=f} = (fromEnum s) * (fromInteger i) -- fromEnum 3.14 == 3 ; fromEnum (-3.14) == -3
+  toEnum int                                                   = BigDecimal {sign = toEnum $ signum int, integerPart = abs $ toInteger int, fractionalPart = [0,0..]}
+  fromEnum BigDecimal{sign=s, integerPart=i, fractionalPart=f} = (fromEnum s) * (fromInteger i) -- fromEnum 3.14 == 3 ; fromEnum (-3.14) == -3
 
 
--- !!! Because the decimal expansion of ALL(!) BigDecimal is infinite, we have to use left-to-right addition !!!
 instance Num BigDecimal where
   -- ===== ===== BigDecimal Addition: ===== =====
   BigDecimal{sign=Minus,integerPart=i1,fractionalPart=f1} + BigDecimal{sign=Minus,integerPart=i2,fractionalPart=f2} = -- (-x)+(-y) == -(x+y)
       negate BigDecimal{sign=Plus,integerPart=i1,fractionalPart=f1} + BigDecimal{sign=Plus,integerPart=i2,fractionalPart=f2} 
   BigDecimal{sign=Minus,integerPart=i1,fractionalPart=f1} + BigDecimal{sign=Plus ,integerPart=i2,fractionalPart=f2} = -- (-x)+y == y+(-x)
       BigDecimal{sign=Plus,integerPart=i2,fractionalPart=f2} + BigDecimal{sign=Minus,integerPart=i1,fractionalPart=f1}
-  BigDecimal{sign=Plus ,integerPart=i1,fractionalPart=f1} + BigDecimal{sign=Plus ,integerPart=i2,fractionalPart=f2} = error "ToDo: x+y"
-  BigDecimal{sign=Plus ,integerPart=i1,fractionalPart=f1} + BigDecimal{sign=Minus,integerPart=i2,fractionalPart=f2} = error "ToDo: x-y"
-
+  x + y -- x+y == x+y+0+0+0+0+0+... -- However, we need to ensure that the absolute value of the numbers given to `evalInfiniteSum` are given in more or less non-ascending order!
+    | integerPart x > integerPart y  = evalInfiniteSum ([x,y] ++ [0,0..])
+    | integerPart y > integerPart x  = evalInfiniteSum ([y,x] ++ [0,0..])
+    | integerPart x /= 0             = evalInfiniteSum ([x,y] ++ [0,0..]) -- the integerParts of x and y are the same but not zero: the order is irrelevant, just use [x,y]
+    | fst digitPair >= snd digitPair = evalInfiniteSum ([x,y] ++ [0,0..]) -- x >= y (x is larger/has less leading zeros and should therefore come first)
+    | otherwise                      = evalInfiniteSum ([y,x] ++ [0,0..]) -- y > x  (y ...)
+    where digitPair = head $ (dropWhile (==(0,0))) $ zip (fractionalPart x) (fractionalPart y) -- the first decimal place where x and y are not both 0
+    -- ToDo/Problem: the above call will never terminate when trying to add 0 to 0 !!!
+    --   The only way to solve this is to actually implement the addition of 2 numbers seperately from `evalInfiniteSum`!
 
   -- ===== ===== BigDecimal Multiplication: ===== =====
   BigDecimal{sign=Minus,integerPart=i1,fractionalPart=f1} * BigDecimal{sign=Plus ,integerPart=i2,fractionalPart=f2} = -- (-x)*y = -(x*y)
@@ -133,14 +161,42 @@ instance Num BigDecimal where
   -- We will split the multiplication into four parts: (i1+f1)*(i2+f2) = i1*i2 + i1*f2 + f1*i2 + f1*f2
   BigDecimal{sign=Plus,integerPart=i1,fractionalPart=f1} * BigDecimal{sign=Plus,integerPart=i2,fractionalPart=f2} =
       BigDecimal{sign=Plus, integerPart=i1*i2, fractionalPart=[0,0..]} -- i1*i2
-    + mulIntFraction i1 f2 -- i1*f2
-    + mulIntFraction i2 f1 -- f1*i2
-    + BigDecimal{sign=Plus, integerPart=0, fractionalPart=mulFractionFraction f1 f1} -- f1*f2
-    where mulIntFraction :: Integer -> [Int] -> BigDecimal
-          mulIntFraction = error "ToDo"
+    + mulIntFraction i1 f2                                             -- i1*f2
+    + mulIntFraction i2 f1                                             -- f1*i2
+    + mulFractionFraction f1 f1                                        -- f1*f2
+    where -- Idea: 12 * 0.123456789... =
+          --       1.2
+          --     + 0.24
+          --     + 0.036
+          --     + 0.0048
+          --     + 0.00060
+          --     + ...
+          --   Evaluate that infinite sum using `evalInfiniteSum` (as it's strictly decreasing!)
+          mulIntFraction :: Integer -> [DecDigit] -> BigDecimal
+          mulIntFraction int fraction = evalInfiniteSum [mul int (fraction!!digitPos) digitPos | digitPos <- [1..]::[Int] ]
+
+          mul :: Integer -> DecDigit -> Int -> BigDecimal -- e.g. mul 12 3 3 == 0.036 (see above)
+          mul intFactor digit position = BigDecimal{sign=Plus, integerPart=error "ToDo", fractionalPart=error "ToDo"}
           
-          mulFractionFraction :: [Int] -> [Int] -> [Int]
-          mulFractionFraction = error "ToDo"
+          -- Idea: 0.232323... * 0.454545... =
+          --       2 * 0.454545... << 1
+          --     + 3 * 0.454545... << 2
+          --     + 2 * 0.454545... << 3
+          --     + 3 * 0.454545... << 4
+          --     + ...
+          --   where "<< n" means shift the decimal point n to the left (i.e. 'divide' by 10^n)
+          --   Evaluate the products using `mulIntFraction` and the infinite sum using `evalInfiniteSum` (it's also strictly decreasing!)
+          mulFractionFraction :: [DecDigit] -> [DecDigit] -> BigDecimal
+          mulFractionFraction frac1 frac2 = evalInfiniteSum [(mulIntFraction (toInteger (frac1!!(digit-1))) frac2) << digit | digit <- [1..]::[Int]]
+          
+          (<<) :: BigDecimal -> Int -> BigDecimal -- shift the decimal point n to the left (i.e. 'divide' by 10^n), e.g. 12.345... << 1 == 1.2345...
+          x << 0 = x
+          BigDecimal{sign=s,integerPart=i,fractionalPart=f} << 1 = BigDecimal{sign=s, integerPart= i `div` 10    , fractionalPart= (fromInteger (i `mod` 10)):f}
+          BigDecimal{sign=s,integerPart=i,fractionalPart=f} << n = BigDecimal{sign=s, integerPart= i `div` (10^n), fractionalPart= (nLastDigits n i)       ++ f}
+          
+          nLastDigits :: Int -> Integer -> [DecDigit] -- e.g. nLastDigits 2 123456 == [5,6]
+          nLastDigits n integer = map (read . (:"")) (show (integer `mod` (10^n))) -- (:"") is to convert '3' to "3" because read expects a String, not a Char
+
 
 
   negate BigDecimal{sign=s,integerPart=i,fractionalPart=f} = BigDecimal {sign = negate s, integerPart = i, fractionalPart = f}
@@ -168,14 +224,31 @@ toRational_precision precision BigDecimal{sign=s, integerPart=i,fractionalPart=f
 instance Fractional BigDecimal where -- one needs to implement either ‘recip’ or ‘/’
   fromRational rational =
     BigDecimal {sign = toEnum $ fromInteger $ signum $ numerator rational, integerPart = abs $ numerator rational, fractionalPart = [0,0..]}
-    / BigDecimal {sign = Plus, integerPart = denominator rational, fractionalPart = [0,0..]} -- as you can see below the denominator of a Rationals is always +
+    / BigDecimal {sign = Plus, integerPart = denominator rational, fractionalPart = [0,0..]} -- as you can see below the denominator of a Rationals is always positve:
   -- numerator   (3 % (-4)) == (-3)
   -- denominator (3 % (-4)) == 4
   -- numerator   ((-3) % (-4)) == 3
   -- denominator ((-3) % (-4)) == 4
 
-  recip BigDecimal{sign=s,integerPart=i,fractionalPart=f} = BigDecimal {sign = s, integerPart = error "ToDo", fractionalPart = error "ToDo"}
-    -- 1/(i+f) = (i-f)/[(i+f)*(i-f)] = (i-f)/[i^2-f^2] -- not helpful :(
+  -- Use the following two Taylor Series expansions for calculating the reciprocal of a BigDecimal: 
+  --   * 1/x     = 1 - (x - 1) + (x - 1)^2 - (x - 1)^3 + (x - 1)^4 - (x - 1)^5 + O((x - 1)^6)    | converges when abs(x-1) < 1 ; expansion at x=1
+  --   * 1/(a+b) = the sum of  (b^n * (-a)^(-n))/a  as n goes from 0 to infinity                 | for abs(b) < abs(a)
+  -- We will use the 1st expansion when the integerPart is 0 (or 1, which is optional) and the 2nd expansion in all other cases.
+  -- When when want to calculate the reciprocal of x=1.99999... (i.e. a=1, b=0.99999...) we got a problem with both series and `recip` shall never terminate :(
+  -- Calculating the reciprocal of x=2.00000... (i.e. a=2, b=0.00000...) works just fine with the second series however as 0.00000... < 2
+  -- 
+  recip x@BigDecimal{sign=s,integerPart=i,fractionalPart=f}
+    | s==Minus            = negate $ recip $ negate x -- 1/(-x) = -(1/x)
+    | i==1 && all (==9) f = 0.5                       -- see above: (recip 1.99999...) shall never terminate as *both* series expansions won't work
+    | i==0 && all (==0) f = undefined                 -- 1/0 is undefined anyways (never returns 'undefined' however as equality is unsolvable for BigDecimals!)
+    | i==0                = evalInfiniteSum [ (-1)^n * (x-1)^n | n <- [0..]::[Int]]               -- use the first series expansion as abs(x-1) < 1
+    | otherwise           = oneOverAplusB i BigDecimal{sign=Plus,integerPart=0,fractionalPart=f}  -- use the second series expansion
+       where oneOverAplusB :: Integer -> BigDecimal -> BigDecimal -- evaluates the Taylor Series for 1/(a+b):
+             oneOverAplusB a b = evalInfiniteSum [ (b^n) * (oneOverInt (-a)^n) * (oneOverInt a) | n <- [0..]::[Int]]
+                          
+             oneOverInt :: Integer -> BigDecimal -- 1/x, takes a positive non-zero Integer, returns the reciprocal as a BigDecimal
+             oneOverInt = error "ToDo: oneOverInt - implement standard division algorithm: 1/Integer -> BigDecimal"
+
 
 
 instance Floating BigDecimal where
@@ -221,17 +294,7 @@ modulo x y
   | otherwise = x
 
 
--- -- -- -- evalInfiniteSum -- -- --
--- e.g. evalInfiniteSum [1.111111111111...,
---                       0.111111111111...,
---                       0.011111111111...,
---                       0.001111111111...,
---                       0.000111111111...,
---                       0.000011111111...,
---                       ...]
---                    == 1.234567...
-evalInfiniteSum :: [BigDecimal] -> BigDecimal
-evalInfiniteSum = error "ToDo"
+
 
 
 
@@ -248,7 +311,43 @@ instance RealFrac BigDecimal where -- properFraction :: (RealFrac a, Integral b)
 
 
 
--- Source: https://oeis.org/A000796 (Reinhard Zumkeller 2013)
+
+
+
+-- -- -- -- -- -- evalInfiniteSum -- -- -- -- --
+-- This is a helper function not only for evaluating the Taylor Series needed for the Floating typeclass but also for (+) and (*) of the Num typeclass and the recip function
+--   of the Fractional typeclass.
+-- Therefore this is the function containing the main (calculation) logic for handling the infinitude of BigDecimals!
+--
+-- e.g. evalInfiniteSum [1.111111111111...,
+--                       0.111111111111...,
+--                       0.011111111111...,
+--                       0.001111111111...,
+--                       0.000111111111...,
+--                       0.000011111111...,
+--                       ...]
+--                    == 1.234567... (which is 100/81 as a fraction by the way)
+--
+-- Because the decimal expansion of ALL(!) BigDecimal is infinite, we have to use left-to-right addition!
+-- The function also has to be able to deal with some of the values being negative!
+-- For simplicity we assume that the given list of BigDecimals is always infinite.
+--   When one wants to sum only finitely many BigDecimals, the rest shall be filled up with infinitely many (BigDecimal) zeros.
+--
+-- In order for this function to work, the absolute value of the numbers in the given list have to be in descending/non-ascending order!
+--   (more or less, i.e. at least in terms of their magnitude/leading zeros)
+-- To be more precise: When one of the numbers begins with n zeros, this function assumes that all the numbers that follow will also begin with at least n zeros!
+--
+evalInfiniteSum :: [BigDecimal] -> BigDecimal
+evalInfiniteSum = error "ToDo: evalInfiniteSum"
+
+
+
+
+
+
+
+
+-- The decimal expansion/digits of Pi | Source: https://oeis.org/A000796 (Reinhard Zumkeller 2013)
 a000796_list = map fromInteger $ piStream (1, 0, 1)
    [(n, a*d, d) | (n, d, a) <- map (\k -> (k, 2 * k + 1, 2)) [1..]] where
    piStream z xs'@(x:xs)
